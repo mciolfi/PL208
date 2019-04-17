fa53b5bdc99326daa3915204b43ac2e5d920f1c1
Marcio Ciolfi <ciolfi@gmail.com>
2019-04-16 23:47:44 -0300

    Update MLP.py

--- 7 - RNA MLP Redes Neurais Artificiais Multilayer Perceptrons/MLP.py
+++ 7 - RNA MLP Redes Neurais Artificiais Multilayer Perceptrons/MLP.py
@@ -1,13 +1,14 @@
 import numpy as np
-from NeuralNetwork import *
-from Neuron import *
 
-Def MLP():
-    Ninput = 4
-    Noutput = 3
-    Nlayer = 2
-    Net = []
-    Nstart = round(Ninput + Noutput) / 2
-    print (Nstart)
-    # Neural Network generation
+Ninput = 4
+Noutput = 3
+Nlayer = 2
+net = []
+Nstart = round((Ninput + Noutput) / 2, 0)
+print (Nstart)
 
+# Neural Network generation
+for layer in range(Nlayer):
+    for neu in range(Ninput):
+        net.append (1)
+print (net)
