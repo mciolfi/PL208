31e72cfa852d8766f3b6a8a7451396053b106527
Marcio Ciolfi <44344801+mciolfi@users.noreply.github.com>
2019-04-17 22:15:38 -0300

    Update MLP.py

--- 7 - RNA MLP Redes Neurais Artificiais Multilayer Perceptrons/MLP.py
+++ 7 - RNA MLP Redes Neurais Artificiais Multilayer Perceptrons/MLP.py
@@ -1,14 +1,18 @@
 import numpy as np
 
-Ninput = 4
-Noutput = 3
-Nlayer = 2
-net = []
-Nstart = round((Ninput + Noutput) / 2, 0)
-print (Nstart)
+# Class definition of a generic neuron
+class Neuron(object):
+    def __init__(self, weights, fnet):
+        self.weights = weights          # Weights array
+        self.fnet = fnet                # Define type of function that represent the neuron function
+        
+def matrix_gen (n_lines, n_columns):
+    return [[((j+1)+i*n_columns) for j in range(n_columns)] for i in range(n_lines)]
 
-# Neural Network generation
-for layer in range(Nlayer):
-    for neu in range(Ninput):
-        net.append (1)
-print (net)
+# Main programm
+Ninp = 4
+Nlay = 3
+Nout = 3
+Nstart = int(round((Ninp + Nout) / 2 , 0))
+Nneurons = matrix_gen (Nlay,Nstart)
+print (Nneurons)
